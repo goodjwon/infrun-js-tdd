17d7fbc5f90dac55e936baf006bc550a94fe2df3
goodjwon <jwon76@naver.com>
2018-10-17 22:35:50 +0900

    보이지 않는 기능 완성

--- ClickCounter/ClickCounter.spec.js
+++ ClickCounter/ClickCounter.spec.js
@@ -1,9 +1,34 @@
 describe('App.ClickCounter', ()=>{
+
+    let counter;
+
+    //준비
+    beforeEach(()=>{
+        counter = App.ClickCounter();
+    })
+
     describe('getValue', ()=>{
         it('초기값이 0인 카운터 값을 반환한다.',()=>{
             //todo
-            const counter = App.ClickCounter();
             expect(counter.getValue()).toBe(0)
         });
     });
+
+    describe('increase()', ()=> {
+        it('카운터 1을 올린다.', ()=>{
+            //준비 => beforEach로 이
+
+
+            //실행
+            const initValue = counter.getValue();
+            counter.increase();
+
+
+            //단언
+            expect(counter.getValue()).toBe(initValue+1);
+
+        })
+    })
 });
+
+
